5ece4d12a0fbac16b5708bd67155daccf9a89576
Tommy Treb <tomtrezb2003@gmail.com>
2022-11-08 11:09:45 -0600

    Update remap

--- nvim/lua/tommytreb/remap.lua
+++ nvim/lua/tommytreb/remap.lua
@@ -1,8 +1,20 @@
-print("Here in remap")
 local nnoremap = require("tommytreb.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
-nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
+
+-- Telescope
+nnoremap("<leader>pf", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<CR>")
 nnoremap("<leader>fb", "<cmd>Telescope buffers<CR>")
 nnoremap("<leader>fh", "<cmd>Telescope help_tags<CR>")
+
+-- Harpoon
+local silent = { silent = true }
+
+nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
+nnoremap("<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
+
+nnoremap("<C-h>", function() require("harpoon.ui").nav_file(1) end, silent)
+nnoremap("<C-j>", function() require("harpoon.ui").nav_file(2) end, silent)
+nnoremap("<C-k>", function() require("harpoon.ui").nav_file(3) end, silent)
+nnoremap("<C-l>", function() require("harpoon.ui").nav_file(4) end, silent)
