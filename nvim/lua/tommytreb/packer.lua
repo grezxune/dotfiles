f3ae9f209d6e0a667c224aa79a73d2cc3688dddb
Tommy Treb <tomtrezb2003@gmail.com>
2022-11-10 12:54:19 -0600

    Add i3 configs

--- nvim/lua/tommytreb/packer.lua
+++ nvim/lua/tommytreb/packer.lua
@@ -5,21 +5,29 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'folke/tokyonight.nvim'
-    use 'EdenEast/nightfox.nvim'
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'},
-            {'nvim-telescope/telescope-fzf-native.nvim'},
-            {'nvim-treesitter/nvim-treesitter'}
-        }
-    }
-    use 'ThePrimeagen/harpoon'
-    use 'ThePrimeagen/git-worktree.nvim'
-    use {
+    use('wbthomason/packer.nvim')
+
+    -- All the things
+    use('nvim-telescope/telescope.nvim')
+    use('nvim-lua/plenary.nvim')
+    use('nvim-telescope/telescope-fzf-native.nvim')
+    use('nvim-treesitter/nvim-treesitter')
+    use('ThePrimeagen/harpoon')
+    use('ThePrimeagen/git-worktree.nvim')
+    use({
         'neoclide/coc.nvim',
         branch = 'release'
-    }
-    use 'jparise/vim-graphql'
+    })
+    use('jparise/vim-graphql')
+    use('neovim/nvim-lspconfig')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/nvim-cmp')
+    use('onsails/lspkind-nvim')
+    use('nvim-lua/lsp_extensions.nvim')
+    use('glepnir/lspsaga.nvim')
+
+    -- Color Schemes
+    use('folke/tokyonight.nvim')
+    use('EdenEast/nightfox.nvim')
 end) 
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -74,6 +74,16 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
   ["coc.nvim"] = {
     loaded = true,
     path = "/home/tommy/.local/share/nvim/site/pack/packer/start/coc.nvim",
@@ -89,10 +99,35 @@ _G.packer_plugins = {
     path = "/home/tommy/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
+  ["lsp_extensions.nvim"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/lsp_extensions.nvim",
+    url = "https://github.com/nvim-lua/lsp_extensions.nvim"
+  },
+  ["lspkind-nvim"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
+    url = "https://github.com/onsails/lspkind-nvim"
+  },
+  ["lspsaga.nvim"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
+    url = "https://github.com/glepnir/lspsaga.nvim"
+  },
   ["nightfox.nvim"] = {
     loaded = true,
     path = "/home/tommy/.local/share/nvim/site/pack/packer/start/nightfox.nvim",
     url = "https://github.com/EdenEast/nightfox.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/tommy/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
