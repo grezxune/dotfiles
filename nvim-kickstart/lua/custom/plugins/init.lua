5e1e6d6dbb0f542a5466476fb8935cd1e99d400e
Tommy Treb <tomtrezb2003@gmail.com>
2024-03-04 08:30:34 -0600

    Break out init.lua and add zettel plugin

--- nvim-kickstart/lua/custom/plugins/init.lua
+++ nvim-kickstart/lua/custom/plugins/init.lua
@@ -1,3 +1,7 @@
+require 'custom.plugins.zettel'
+require 'custom.plugins.autopairs'
+require 'custom.plugins.markdown_preview'
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
